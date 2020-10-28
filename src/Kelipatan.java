db903ab67cadf0420c4e3c964b7926e2620e76c9
diky <diky@diky-PC>
2020-10-28 15:27:29 +0700

    Upload Project

---
+++ src/Kelipatan.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Diva Sabila Ramadhan
+ * Nama  : Diva Sabila Ramadhan
+ * Kelas : IF1
+ * NIM   : 10119039
+ * DEskripsi Program : Menampilkan bilangan kelipatan 3.5 s/d 35.
+ */
+public class Kelipatan {
+
+    public static void main(String[] args) {
+        double i;
+        for(i=0;i<=35;i+=3.5) {
+            System.out.println(i);
+        }
+    }
+    
+}
